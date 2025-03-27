bfe48e37b0301ba515c5abcd7598259200c5d85b
Aieyan Helium <aieyan@iamhelium.com.au>
2025-03-27 14:17:31 +1100

    Added eslint

--- ui.frontend/.storybook/preview.ts
+++ ui.frontend/.storybook/preview.ts
@@ -1,6 +1,5 @@
 import type { Preview } from '@storybook/react'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-import { themes } from '@storybook/theming'
 import yamahaTheme from './yamahaTheme'
 import '../src/assets/generated-icons/icons.scss'
 import '../src/styles/main.scss'
