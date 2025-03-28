4eb39f208eeb3ad2218128fa37e612cea3dd85fe
Aieyan Helium <aieyan@iamhelium.com.au>
2025-03-28 13:06:55 +1100

    Applied prettier to all files

--- ui.frontend/.storybook/stories.config.ts
+++ ui.frontend/.storybook/stories.config.ts
@@ -1,21 +1,21 @@
 // Add storybook configurations here.
 
 export const parameters = ({ url }: { url: string }) => {
-  return {
-    design: [
-      {
-        type: "figspec",
-        // accessToken: process.env.STORYBOOK_DESIGN_TOKEN,
-        url,
-      },
-      {
-        type: "figma",
-        url,
-      },
-      {
-        type: "link",
-        url,
-      }
-    ],
-  };
-};
+   return {
+      design: [
+         {
+            type: 'figspec',
+            // accessToken: process.env.STORYBOOK_DESIGN_TOKEN,
+            url,
+         },
+         {
+            type: 'figma',
+            url,
+         },
+         {
+            type: 'link',
+            url,
+         },
+      ],
+   }
+}
