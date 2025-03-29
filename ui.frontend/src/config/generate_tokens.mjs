4ea5267cdae702a5d0387745090d4f00e20d6e43
Diego Azevedo <100555348+diego-helium@users.noreply.github.com>
2025-03-29 15:29:05 +1100

    Feat/supernova tokens (#31)
    * chore: update design tokens and add glob dependency
    
    - Added glob dependency to package.json and package-lock.json.
    - Introduced new configuration files for Style Dictionary to manage design tokens.
    - Created new token files for borders, core colors, font sizes, spacing, and semantics.
    - Updated style-dictionary configuration to include new transform hooks.
    - Generated SCSS files for the new design tokens.
    
    * files deleted
    
    * feat: introduce token generation script and restructure design tokens
    
    - Added a new script to generate separate token files for color, dimension, spacing, and typography categories.
    - Removed outdated transform scripts to streamline the token management process.
    - Updated the main token JSON file to include new dimensions and color categories, ensuring better organization and accessibility.
    
    * feat: enhance token generation and file management
    
    - Added new scripts for creating separate token files and generating tokens from JSON sources.
    - Updated paths in the token generation script to resolve relative directory issues.
    - Improved logging for better visibility during token generation and file creation processes.
    
    * comments removed
    
    * fix: update GitHub Actions workflow for token generation
    
    - Changed the trigger from push to pull_request for the token generation workflow.
    - Updated the token generation command to create token files and generate tokens in a single step.

---
+++ ui.frontend/src/config/generate_tokens.mjs
@@ -0,0 +1,43 @@
+import { globSync } from 'glob'
+import StyleDictionary from 'style-dictionary'
+import { dirname, resolve } from 'node:path/posix'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const sourceDir = resolve(__dirname, '../styles/tokens/auto_source')
+const outputDir = resolve(__dirname, '../styles/tokens/auto_output')
+
+const tokenFiles = globSync(`${sourceDir}/*.json`)
+
+const myStyleDictionary = new StyleDictionary({
+   source: tokenFiles,
+   platforms: {
+      scss: {
+         transformGroup: 'scss',
+         buildPath: `${outputDir}/`,
+         files: tokenFiles.map((file) => {
+            const fileName = file.split('/').pop() // Get just the filename
+            return {
+               destination: fileName.replace('.json', '.scss'),
+               format: 'scss/variables',
+               filter: (token) => token.filePath.endsWith(fileName),
+               options: {
+                  outputReferences: !fileName.includes('color'),
+               },
+            }
+         }),
+      },
+   },
+   hooks: {
+      transformGroups: {
+         scss: ['size/pxToRem'],
+      },
+   },
+})
+
+console.log('Generating tokens from:', sourceDir)
+console.log('Output directory:', outputDir)
+
+myStyleDictionary.buildAllPlatforms()
---
+++ ui.frontend/src/utils/parsers/create_single_files.mjs
@@ -0,0 +1,72 @@
+import { readFile, writeFile, mkdir } from 'fs/promises'
+import { dirname, resolve } from 'node:path/posix'
+import { fileURLToPath } from 'url'
+
+async function createSeparateFiles() {
+   console.log('Creating separate files for tokens')
+   const __filename = fileURLToPath(import.meta.url)
+   const __dirname = dirname(__filename)
+
+   const sourceTokensPath = resolve(
+      __dirname,
+      '../../../src/styles/tokens/source/tokens.json'
+   )
+   const outputDir = resolve(
+      __dirname,
+      '../../../src/styles/tokens/auto_source'
+   )
+   const tokens = JSON.parse(await readFile(sourceTokensPath, 'utf-8'))
+   const { color, dimension } = tokens
+   const colorCategories = {
+      color: {
+         color: {
+            core: color.core,
+         },
+      },
+      surface: {
+         surface: color.surface,
+      },
+      border: {
+         border: { ...color.border, ...dimension.border },
+      },
+      text: {
+         text: color.text,
+      },
+      content: {
+         content: color.content,
+      },
+      spacing: {
+         spacing: dimension.spacing,
+      },
+      padding: {
+         padding: dimension.padding,
+      },
+      fontsize: {
+         fontsize: dimension['font-size'],
+      },
+   }
+
+   const persistColorCategory = async ([categoryName, categoryTokens]) => {
+      console.log(`Processing ${categoryName} category`)
+      const fileName = resolve(outputDir, `${categoryName}.json`).toLowerCase()
+
+      const dirName = dirname(fileName)
+      try {
+         await mkdir(dirName, { recursive: true })
+      } catch (e) {
+         console.log('dir already exists')
+      }
+      await writeFile(
+         fileName,
+         JSON.stringify(categoryTokens, null, 2).toLowerCase(),
+         'utf-8'
+      )
+   }
+
+   // Create separate files for each category
+   await Promise.all(Object.entries(colorCategories).map(persistColorCategory))
+
+   console.log('Separate files created successfully')
+}
+
+createSeparateFiles().catch(console.error)
